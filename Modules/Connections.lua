32c1fae2d9b4cff642f25f3878ec044c60c1f8df
dev.pawaret <dev.pawaret@gmail.com>
2026-02-07 01:56:23 +0700

    Add Module Connections

---
+++ Modules/Connections.lua
@@ -0,0 +1,29 @@
+local Connections = {}
+Connections._storage = {}
+
+function Connections:Connect(signal, cb, tag)
+    tag = tag or "Default"
+
+    local conn = signal:Connect(cb)
+
+    if not Connections._storage[tag] then
+        Connections._storage[tag] = {}
+    end
+
+    table.insert(Connections._storage[tag], conn)
+    return conn
+end
+
+function Connections:Clean(tag)
+    tag = tag or "Default"
+
+    local list = Connections._storage[tag]
+    if next(list) then
+        for _, conn in ipairs(list) do
+            if conn.Connected then conn:Disconnect() end
+        end
+        Connections._storage[tag] = nil
+    end
+end
+
+return Connections
